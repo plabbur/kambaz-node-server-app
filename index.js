3ab9ff9442cd7a21d66392b1c1b16624bae66079
Cole <coleabrams11@gmail.com>
2025-12-07 17:11:35 -0500

    please

--- index.js
+++ index.js
@@ -35,7 +35,6 @@ if (process.env.SERVER_ENV !== "development") {
   sessionOptions.cookie = {
     sameSite: "none",
     secure: true,
-    domain: process.env.SERVER_URL,
   };
 }
 app.use(session(sessionOptions));
